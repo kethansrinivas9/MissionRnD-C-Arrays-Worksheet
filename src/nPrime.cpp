caed91fd07e00219aa978281b0cf7ce79cab37f5
kethan_dasari <kethansrinivas9@gmail.com>
2016-09-26 00:08:17 +0530

    added new array files

---
+++ src/nPrime.cpp
@@ -0,0 +1,19 @@
+
+/*
+OVERVIEW: Given a number N return all the prime numbers upto the integer N.
+Ex: input: 10 output: [2,3,5,7] 
+
+INPUTS: An Integer N
+
+OUTPUT: Prime numbers until the number N(N inclusive)
+
+ERROR CASES: Return NULL for invalid inputs
+
+NOTES:
+*/
+
+#include <stdio.h>
+int* nPrime(int N)
+{
+	return NULL;
+}
